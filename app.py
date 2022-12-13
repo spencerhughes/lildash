093eef1da766a6cb672237cfc81a2a367617110e
spencerhughes <16389711+spencerhughes@users.noreply.github.com>
2022-12-13 09:32:40 -0500

    Adds functions to interact with docker

--- app.py
+++ app.py
@@ -17,3 +17,15 @@ client = docker.from_env()
 app = flask.Flask(__name__)
 app.config.from_mapping(config)
 cache = flask_caching.Cache(app)
+
+# Functions
+
+def get_containers():
+	return client.containers.list(filters={'label': top_level_config_key + '.' + enable_config_key, 'status': 'running'})
+
+@cache.cached(timeout=60, key_prefix='docker_containers')
+def gen_container_list():
+	containers = {}
+	for container in get_containers():
+		containers[container.name] = container.labels[top_level_config_key + '.' + enable_config_key]
+	return containers
